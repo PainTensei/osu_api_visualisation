734e8aec2da918443dcbd2c38d03972baf039908
Ian Levy <43404217+PainTensei@users.noreply.github.com>
2020-01-19 15:10:19 +0100

    update
    added the value's for the graph now LIVE from the API data

--- code3.js
+++ code3.js
@@ -1,4 +1,11 @@
-var index = ["WhiteCat", "Vaxei", "Alumetri", "FlyingTuna", "idke", "Micca", "Karthy", "Freddie Benson", "Ryuk", "Varvalian", "a_Blue", "FGSky", "Mathi", "ASecretBox", "firebat92", "Vamhi", "Rafis", "fieryrage", "Dereban", "chocomint"];
+
+
+var index = ["WhiteCat", "Vaxei", "Alumetri", "FlyingTuna", "idke", "Micca", "Karthy", "Freddie Benson", "Ryuk", "Varvalian"
+//, "a_Blue", "FGSky", "Mathi", "ASecretBox", "firebat92", "Vamhi", "Rafis", "fieryrage", "Dereban", "chocomint"
+];
+console.log(index); // 10
+
+var seconds = [];
 
 var request = new XMLHttpRequest();
 
@@ -13,20 +20,20 @@ var request = new XMLHttpRequest();
         if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
             var response = request.response;
             var data = JSON.parse(response);
-            console.log(data[0].playcount);
+            seconds.push(data[0].total_seconds_played);
+            console.log(seconds);
 
             var myChart = document.createElement('li');
 
             for(item in data[0]) {
                 if (item == 'total_seconds_played'){
-                    myChart.innerHTML += index[i] + ' ' + item + ': ' + data[0][item] + ' sec';  
+                    myChart.innerHTML += index[i] + ' ' + item + ': ' + data[0][item] + ' sec';
                 }
             }
 
             document.body.appendChild(myChart);
             myChart.innerHTML += '<br>';
             loop(i + 1, length);
-            // console.log(myChart);
         }
     }
     request.send();
@@ -42,17 +49,17 @@ var request = new XMLHttpRequest();
 
 
 
-
-
 var ctx = document.getElementById('chart').getContext('2d');
 var myChart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: ["1. WhiteCat", "2. Vaxei", "3. Alumetri", "4. FlyingTuna", "5. idke", "6. Micca", "7. Karthy", "8. Freddie Benson", "9. Ryuk", "10. Varvalian", "11. a_Blue", "12. FGSky", "13. Mathi", "14. ASecretBox", "15. firebat92", "16. Vamhi", "17. Rafis", "18. fieryrage", "19. Dereban", "20. chocomint"],
+        labels: ["1. WhiteCat", "2. Vaxei", "3. Alumetri", "4. FlyingTuna", "5. idke", "6. Micca", "7. Karthy", "8. Freddie Benson", "9. Ryuk", "10. Varvalian"
+        //, "11. a_Blue", "12. FGSky", "13. Mathi", "14. ASecretBox", "15. firebat92", "16. Vamhi", "17. Rafis", "18. fieryrage", "19. Dereban", "20. chocomint"
+        ],
 
         datasets: [{
             label: 'Total seconds played',
-            data: [420377, 6430274, 3710839, 5262409, 5368404, 4667205, 10825707, 3755838, 7912683, 11395787, 5002677, 9557455, 7949605, 7425159, 6502881, 4699347, 11460073, 6200754, 6669271, 2441013],
+            data: seconds,
             backgroundColor:'rgba(54, 200, 235, 0.2)',
             borderColor:'rgba(0, 0, 0, 1)',
             borderWidth: 1
