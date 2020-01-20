92f13cb1b0d2555d5090fb6bdabb796f65c5a76c
Ian Levy <43404217+PainTensei@users.noreply.github.com>
2020-01-20 14:42:09 +0100

    request the api key
    removed my own key

--- code1.js
+++ code1.js
@@ -1,32 +1,34 @@
 var request = new XMLHttpRequest();
 
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=WhiteCat&m=0'); //1
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Vaxei&m=0'); //2
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Alumetri&m=0'); //3
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=FlyingTuna&m=0'); //4
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=idke&m=0'); //5
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Micca&m=0'); //6
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Karthy&m=0'); //7
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=7342622&m=0'); //8
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=RyuK&m=0'); //9
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Varvalian&m=0'); //10
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=a_Blue&m=0'); //11
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=FGSky&m=0'); //12
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Mathi&m=0'); //13
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=ASecretBox&m=0'); //14
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=firebat92&m=0'); //15
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Vamhi&m=0'); //16
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Rafis&m=0'); //17
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=fieryrage&m=0'); //18
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Dereban&m=0'); //19
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=chocomint&m=0'); //20
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=spare&m=0'); //21
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=badeu&m=0'); //22
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Weabole&m=0'); //23
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=Dustice&m=0'); //24
-request.open('GET', 'https://osu.ppy.sh/api/get_user?k=cd0c48e95ce8bd3a80fe7c2620dd59d426dcba26&u=jpeg&m=0'); //25
+var apikey = prompt("Please enter your osu! api key");
+
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=WhiteCat&m=0"); //1
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Vaxei&m=0"); //2
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Alumetri&m=0"); //3
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=FlyingTuna&m=0"); //4
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=idke&m=0"); //5
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Micca&m=0"); //6
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Karthy&m=0"); //7
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=7342622&m=0"); //8
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=RyuK&m=0"); //9
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Varvalian&m=0"); //10
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=a_Blue&m=0"); //11
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=FGSky&m=0"); //12
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Mathi&m=0"); //13
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=ASecretBox&m=0"); //14
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=firebat92&m=0"); //15
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Vamhi&m=0"); //16
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Rafis&m=0"); //17
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=fieryrage&m=0"); //18
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Dereban&m=0"); //19
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=chocomint&m=0"); //20
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=spare&m=0"); //21
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=badeu&m=0"); //22
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Weabole&m=0"); //23
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=Dustice&m=0"); //24
+request.open('GET', "https://osu.ppy.sh/api/get_user?k=" + apikey + "&u=jpeg&m=0"); //25
 // Base URL = https://osu.ppy.sh/api/
-// k = API_key
+// k = apikey
 // u = username (or ID)
 // m = gamemode (0 to 3)
 
